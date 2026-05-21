20db5e93c8068f0e6d76730409b0b3ae0a881715
shroida <shroida012@gmail.com>
2026-05-21 10:34:14 +0300

    replace

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:riverpod_app/screens/home_screen.dart';
+import 'package:riverpod_app/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
