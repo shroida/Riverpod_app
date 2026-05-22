7cee282814a54db3f38514db2674c61121cc5112
shroida <shroida012@gmail.com>
2026-05-22 09:13:06 +0300

    Selected Products

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -18,7 +18,7 @@ class _CartScreenState extends ConsumerState<CartScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Your Cart'),
+        title: const Text('Selected Products'),
         centerTitle: true,
         // actions: [],
       ),
