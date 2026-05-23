d0ce8dc4c2a3652d716a7f66951ab94e544f4f01
shroida <shroida012@gmail.com>
2026-05-23 14:58:11 +0300

    Add cart length

--- lib/screens/shared/cart_icon.dart
+++ lib/screens/shared/cart_icon.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/providers/cart_provider.dart';
 import 'package:riverpod_app/screens/cart/cart_screen.dart';
 
-class CartIcon extends StatelessWidget {
+class CartIcon extends ConsumerWidget {
   const CartIcon({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Stack(
       children: [
         IconButton(
@@ -26,6 +28,14 @@ class CartIcon extends StatelessWidget {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
               color: Colors.blueAccent,
+            ),
+            child: Text(
+              '${ref.watch(cartProvider).length}',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 10,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
